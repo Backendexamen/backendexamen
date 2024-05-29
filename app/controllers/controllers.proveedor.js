5a7616db66a3a03fc8eb72d017d172e37b6adea3
daniela rojas <danirojasbermudez20@gmail.com>
2024-05-29 08:17:25 -0500

    creacion de controllers, conexion con base de datos

---
+++ app/controllers/controllers.proveedor.js
@@ -0,0 +1,62 @@
+import pool from "../config/db.config.js";
+import { completado, error } from "../message/mensajes.js";
+import bcrypt from "bcrypt";
+
+export const mostrarproveedores = async(req,res)=>{
+    try {
+        const respuesta = await pool.query(`CALL sp_mostrarproveedores()`);
+        completado(req,res, 200, respuesta[0]);
+        
+    } catch (err) {
+        error(req,res, 500, err)
+    }
+};
+
+export const insertarproveedor = async(req,res)=>{
+    const nombre = req.body.nombre;
+    const contacto =req.body.contacto;
+    const empresa=req.body.empresa;
+    try {
+        const respuesta = await pool.query(`CALL sp_insertarproveedor('${nombre}', '${contacto}', '${empresa}')`)
+        completado(req,res,200,"usuario insertado correctamente")
+    } catch (err) {
+        error(req,res,200,"usuario insertado incorrecto")
+    }
+};
+
+export const mostrarproveedor =async(req,res)=>{
+    const id =req.params['id'];
+    try {
+        const respuesta = await pool.query(`CALL sp_mostrarproveedor(${id})`);
+        completado(req,res,200,respuesta[0])
+    } catch (err) {
+        error(req,res,500,err)
+    }
+};
+
+export const eliminarproveedor =async(req,res)=>{
+    const id =req.params['id'];
+    try {
+        const respuesta = await pool.query(`CALL sp_eliminarproveedor(${id})`);
+        completado(req,res,200,respuesta[0])
+    } catch (err) {
+        error(req,res,500,err)
+    }
+};
+
+export const modificarproveedor = async(req,res)=>{
+    const id = req.body.id;
+    const nombre = req.body.nombre;
+    const contacto =req.body.contacto;
+    const empresa=req.body.empresa;
+    try {
+        const respuesta = await pool.query(`CALL sp_modificarproveedor(${id}, '${nombre}', '${contacto}','${empresa}')`);
+        if (respuesta[0].affectedRows==1) {
+            completado(req,res,200,"usuario modificado");
+        } else {
+            completado(req,res,200,"no se pudo modificar");
+        }
+    } catch (err) {
+        error(req,res, 400, err)
+    }
+};
--- app/routes/routes.proveedor.js
+++ app/routes/routes.proveedor.js
@@ -3,4 +3,10 @@ import { Router } from "express";
 
 const rutaproveedor = Router();
 
+rutaproveedor.post("/proveedor" , insertarproveedor);
+rutaproveedor.get("/proveedor", mostrarproveedores);
+rutaproveedor.put("/proveedor", modificarproveedor);
+rutaproveedor.get("/proveedor/:id",mostrarproveedor);
+rutaproveedor.delete("/proveedor/:id",eliminarproveedor);
+
 export default rutaproveedor;
